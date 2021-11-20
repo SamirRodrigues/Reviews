f59e5ab5813492427ce95f947ecd5a2c4f27dab9
SamirRodrigues <emaildesamir@gmail.com>
2021-11-20 01:49:08 -0300

    deploy

---
+++ build/precache-manifest.5576ce55387ec181bb06513d61fe4949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85031465f22398603bb38e0fa5d8d90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffbd40670de070968ef9",
+    "url": "/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "61c9a72a86221f692a54",
+    "url": "/static/js/2.611ee867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.611ee867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbd40670de070968ef9",
+    "url": "/static/js/main.baca1062.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
