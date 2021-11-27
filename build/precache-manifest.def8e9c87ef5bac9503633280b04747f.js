27ad532fe2e50c2bce31298c80dcfb7f56387351
SamirRodrigues <emaildesamir@gmail.com>
2021-11-26 21:48:36 -0300

    deploy

---
+++ build/precache-manifest.def8e9c87ef5bac9503633280b04747f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79944841ef63882823af627177d254f",
+    "url": "/Reviews/index.html"
+  },
+  {
+    "revision": "ca16baa443bc89461579",
+    "url": "/Reviews/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/Reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca16baa443bc89461579",
+    "url": "/Reviews/static/js/main.daeaaecb.chunk.js"
+  },
+  {
+    "revision": "5cfec122a862e1e7b69a",
+    "url": "/Reviews/static/js/runtime-main.949f2259.js"
+  }
+]);
