50c96a06addf89cd52ca0008e321871cceb7d06c
SamirRodrigues <emaildesamir@gmail.com>
2021-11-26 21:46:52 -0300

    deploy

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,68 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = people[index]
 
-export default Review;
+  const checkNewIndex = value => {
+    if (value > people.length - 1) {
+      return 0
+    } else if (value < 0) {
+      return people.length - 1
+    } else {
+      return value
+    }
+  }
+
+  const nextPerson = () => {
+    setIndex(index => {
+      let newIndex = index + 1
+      return checkNewIndex(newIndex)
+    })
+  }
+
+  const prevPerson = () => {
+    setIndex(index => {
+      let newIndex = index - 1
+      return checkNewIndex(newIndex)
+    })
+  }
+
+  const randomPerson = () => {
+    let newIndex = Math.floor(Math.random() * people.length)
+
+    if (newIndex === index) {
+      newIndex = index + 1
+    }
+    setIndex(checkNewIndex(newIndex))
+  }
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
+    </article>
+  )
+}
+
+export default Review
